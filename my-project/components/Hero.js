a69497a523eb4822eaa3abcdd3dc8d22bb767a53
JoseSolorio <lodisolito@gmail.com>
2022-11-19 19:18:25 -0800

    fixed responsive on phone
    the hero h1 was making the div height change and was causing layout shift on mobile. making the site unreadable.. usability issue.

--- my-project/components/Hero.js
+++ my-project/components/Hero.js
@@ -3,14 +3,14 @@ import ReactRotatingText from "react-rotating-text"
 
 export default function Hero() {
   return (
-    <div className=" bg-blue-500">
-        <div className="container mx-auto min-h-[40rem] grid grid-cols-1 md:grid-cols-2 pt-16  space-y-10 md:space-x-5">
-            <div className="flex flex-col  pt-14 space-y-10 pl-5">
+    <div className=" bg-blue-500 pb-24  ">
+        <div className="container mx-auto min-h-[24rem] grid grid-cols-1 md:grid-cols-2 pt-16  space-y-10 md:space-x-5">
+            <div className="flex flex-col  pt-14  space-y-10 pl-5 min-h-[28rem] h-[28rem] max-h-[28rem]">
                 <h1 className="text-[4rem] font-bold max-w-xs">Full-stack <ReactRotatingText items={['React Developer', 'React Developer', 'React Developer']} />  </h1>
                 <p className="text-lg">Working in an agile cross-functional environment is what makes a great product!</p>
             </div>
 
-            <div className="relative h-96 ml-0 pl-0 ">
+            <div className="relative min-h-96 h-96 max-h-96 ml-0 pl-0 ">
                 <Image
                 src="/profile.jpg"
                 layout="fill"
