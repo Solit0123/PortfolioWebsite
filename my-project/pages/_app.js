e86beffd1624d1ba396af2416e3d0186744c71a9
JoseSolorio <lodisolito@gmail.com>
2023-01-19 21:20:25 -0800

    Update _app.js

--- my-project/pages/_app.js
+++ my-project/pages/_app.js
@@ -7,25 +7,36 @@ function MyApp({ Component, pageProps }) {
     
   <div>
     <Head>
+    
+
+    </Head>
     <Script
       id="show-banner"
       strategy="afterInteractive"
       dangerouslySetInnerHTML={{
         __html: `
-        
         window.markerConfig = {
-          project: '63ca1870671d2395d00ecce7', 
-          source: 'snippet'
-        };
+          project: '63ca1870671d2395d00ecce7',
+          source: 'snippet',
+        
+          // Identify your reporter
+          reporter: {
+            email: 'john@clientwebsite.com',
+            fullName: 'John Smith',
+          },
+        
+          // Collect custom information
+          customData: {
+            storeId: 123,
+            storeName: 'Organic Fruits',
+          },
+        }
       
       !function(e,r,a){if(!e.__Marker){e.__Marker={};var t=[],n={__cs:t};["show","hide","isVisible","capture","cancelCapture","unload","reload","isExtensionInstalled","setReporter","setCustomData","on","off"].forEach(function(e){n[e]=function(){var r=Array.prototype.slice.call(arguments);r.unshift(e),t.push(r)}}),e.Marker=n;var s=r.createElement("script");s.async=1,s.src="https://edge.marker.io/latest/shim.js";var i=r.getElementsByTagName("script")[0];i.parentNode.insertBefore(s,i)}}(window,document);
       
         `,
       }}
     />
-
-    </Head>
-  
   <Component {...pageProps} />
 </div>
   )
