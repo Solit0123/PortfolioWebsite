8a900fb39e9dd94614f5860c6716f5816e920dd9
JoseSolorio <lodisolito@gmail.com>
2023-01-24 22:19:56 -0800

    zxzxzxz

--- my-project/components/Experience.js
+++ my-project/components/Experience.js
@@ -2,16 +2,29 @@
 
 function Experience() {
   return (
-    <div className=" bg-blue-100">
+    <div className=" bg-black text-white">
 
     
       <div className="container mx-auto flex flex-col text-center space-y-4 py-16">
-          <h2 className="text-5xl font-semibold ">Experience</h2>
-          <p className="text-md">Check out my LinkedIn for my previous experience </p>
+       
+        <h2 className="text-5xl font-semibold ">Experience</h2>
+          <p className="text-md">Check out my LinkedIn for my previous experience, <br/>
+         </p>
           <a className="" href="https://www.linkedin.com/in/jose-santos-solorio-315a811b7/" target="_blank">
               <img src="./linkedin.svg" width="45px" height="45px" alt="" className="mx-auto hover:scale-110 hover:shadow-lg transition duration-150 ease-out" />
           </a>
+          <p className="">
+            
+           or contact me at: <a href="mailto:lodisolito@gmail.com">
+           lodisolito@gmail.com
+        </a>
+          </p>
+        <a href="mailto:lodisolito@gmail.com" className="border w-max mx-auto p-4 mx-auto hover:scale-110 hover:shadow-lg transition duration-150 ease-out">
+          EMAIL ME
+        </a>
+
         
+          
       </div>
     </div>
   )
--- my-project/pages/index.js
+++ my-project/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
     <Header2/>
 
     {/* <Hero/> */}
-    {/* <Experience/> */}
+    <Experience/>
     {/* <Projects/> */}
     {/* <OtherProjects/> */}
     {/* <Footer/> */}
